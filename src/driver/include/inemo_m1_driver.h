59b61a46ee13bac044fb3d3a5312e1978f58b071
Myzhar <myzhar@robot-home.it>
2014-11-04 20:05:55 +0100

    Added Qt5 to use QThread  \ Does not link... error with vtable!

--- src/driver/include/inemo_m1_driver.h
+++ src/driver/include/inemo_m1_driver.h
@@ -1,6 +1,7 @@
 #ifndef _INEMO_M1_DRIVER_H_
 #define _INEMO_M1_DRIVER_H_
 
+#include <QThread>
 #include <ros/ros.h>
 #include <serial/serial.h>
 #include <stdlib.h>
@@ -11,14 +12,29 @@ using namespace std;
 namespace inemo
 {
 
-class CInemoDriver
+class CInemoDriver : public QThread
 {
+    Q_OBJECT
 
 public:
     CInemoDriver();
     ~CInemoDriver();
 
     bool startIMU();
+    bool stopIMU();
+
+protected:
+    // >>>>> iNemo Protocol
+    // <<<<< iNemo Protocol
+
+    /*!
+     * \brief run
+     * Process the serial data continously when
+     * the \ref iNEMO_Start_Acquisition is called
+     *
+     */
+    void run() Q_DECL_OVERRIDE;
+    void processSerialData( string& data );
 
 private:
     ros::NodeHandle m_nh;
@@ -28,7 +44,11 @@ private:
     string mSerialPort;
     uint32_t mBaudrate;
     uint32_t mTimeout;
-}
+
+    bool mStopped; ///< Used to stop the serial processing
+
+    uint64_t mNoDataCounter;
+};
 
 }
 
--- src/driver/src/inemo_m1_driver.cpp
+++ src/driver/src/inemo_m1_driver.cpp
@@ -1,15 +1,17 @@
 #include <inemo_m1_driver.h>
 
+
 namespace inemo
 {
 
-CInemoDriver::CInemoDriver()
+CInemoDriver::CInemoDriver() :
+    QThread( NULL )
 {
     // TODO: add parameters for serial port initialization!!!
 
     mSerialPort = "/dev/ttyUSB0";
     mBaudrate = 56000;
-    mTimeout = 500;
+    mTimeout = 100;
 
 }
 
@@ -45,9 +47,52 @@ bool CInemoDriver::startIMU()
         return false;
     }
 
-    // TODO start timers!
+    // TODO call iNEMO_Start_Acquisition that "start"s the thread if serial reply is ACK
 
     return true;
+}
+
+bool CInemoDriver::stopIMU()
+{
+    mStopped = true;
+}
+
+void CInemoDriver::run()
+{
+    mStopped = false;
+    mNoDataCounter = 0;
+
+    ROS_INFO_STREAM( "IMU Data acquring loop started");
+
+    while(!mStopped)
+    {
+        ros::spinOnce(); // Processing ROS messages
+
+        if( mSerial.available() )
+        {
+            ROS_DEBUG_STREAM("Reading from serial port");
+
+            string data = mSerial.read(mSerial.available());
+
+            ROS_DEBUG_STREAM("Read: " << data);
+
+            processSerialData( data );
+        }
+        else
+        {
+            mNoDataCounter++;
+            ROS_INFO_STREAM( "No IMU data since more than " << mNoDataCounter * mTimeout << " msec" );
+        }
+
+    }
+
+    ROS_INFO_STREAM( "IMU Data acquring loop stopped");
+}
+
+void CInemoDriver::processSerialData( string& data )
+{
+    // TODO Process iNEMO_Acquisition_Data message
+    // to extract attitude and quaternion
 }
 
 }
